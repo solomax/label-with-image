d8cb42394e78079276e067418d416bdf107ccf71
Maxim Solodovnik <solomax666@gmail.com>
2017-10-25 10:48:10 +0700

    Wicket quickstart with text with image

---
+++ src/main/java/org/apache/solomax/HomePage.java
@@ -0,0 +1,18 @@
+package org.apache.solomax;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class HomePage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+	public HomePage(final PageParameters parameters) {
+		super(parameters);
+
+		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+
+		String htmlWithImgTag = "some text at the beginning <img src=\"images/icon/add.png\">   something text at the end";
+		add(new Label("message", htmlWithImgTag).setEscapeModelStrings(false));
+	}
+}
