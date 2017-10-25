e4bc0e564d9f108cc0e392fcd350f41bf893678f
Maxim Solodovnik <solomax666@gmail.com>
2017-10-25 23:57:02 +0700

    Additional page is added

---
+++ src/main/java/org/apache/solomax/DetailPage.java
@@ -0,0 +1,16 @@
+package org.apache.solomax;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class DetailPage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+	public DetailPage(final PageParameters parameters) {
+		super(parameters);
+
+		String htmlWithImgTag = "some DETAIL text at the beginning <img src=\"/images/icon/add.png\">   something text at the end";
+		add(new Label("message", htmlWithImgTag).setEscapeModelStrings(false));
+	}
+}
--- src/main/java/org/apache/solomax/HomePage.java
+++ src/main/java/org/apache/solomax/HomePage.java
@@ -2,6 +2,7 @@ package org.apache.solomax;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
@@ -12,7 +13,16 @@ public class HomePage extends WebPage {
 
 		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
 
-		String htmlWithImgTag = "some text at the beginning <img src=\"images/icon/add.png\">   something text at the end";
+		String htmlWithImgTag = "some text at the beginning <img src=\"/images/icon/add.png\">   something text at the end";
 		add(new Label("message", htmlWithImgTag).setEscapeModelStrings(false));
+
+		add(new Link<Void>("goDetail") {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick() {
+				setResponsePage(DetailPage.class);
+			}
+		});
 	}
 }
